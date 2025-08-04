26ba87579b81fdfd87d1f30e71a7c61d0295f13b
Mostafa Elkaranshawy <mostafaelkaranshawy0@gmail.com>
2025-08-04 23:47:32 +0300

    feat:     - Added JWT Authentication     - Added Database Connection `JPA with JDBC:mysql driver`     - Add Project Structure

---
+++ Backend/src/main/java/com/project/project/Models/Token.java
@@ -0,0 +1,40 @@
+package com.project.project.Models;
+
+import java.util.Date;
+
+public class Token {
+    private String token;
+    private Date expiration;
+    private String username;
+
+    public Date getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(Date expiration) {
+        this.expiration = expiration;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Token() {
+    }
+
+    public Token(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
---
+++ Backend/src/main/java/com/project/project/Models/User.java
@@ -0,0 +1,48 @@
+package com.project.project.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Entity
+public class User {
+    @Id
+    private int id;
+
+    private String username;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
